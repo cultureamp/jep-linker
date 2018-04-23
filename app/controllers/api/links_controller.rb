25b4f98f01bebb55e396c933cfe473e8fd9e4de7
Ryan Bigg <ryanbigg@cultureamp.com>
2018-04-23 10:06:41 +1000

    Week 14 - Part 3

--- app/controllers/api/links_controller.rb
+++ app/controllers/api/links_controller.rb
@@ -2,17 +2,26 @@ class Api::LinksController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @links = Link.all
+    @links = @user.links.all
+  end
+
+  def create
+    @link = @user.links.create(link_params)
+    render "show"
   end
 
   private
 
+  def link_params
+    params.require(:link).permit(:long_url, :short_url)
+  end
+
   def authenticate_user
-    user = authenticate_with_http_token do |token|
+    @user = authenticate_with_http_token do |token|
       User.find_by(token: token)
     end
 
-    unless user
+    unless @user
       render json: { error: "Unauthenticated." }, status: 401
       false
     end
