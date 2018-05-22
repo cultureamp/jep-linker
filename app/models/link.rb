2f07a5c890ffc40bcbf149afb6f31217b75bbb55
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-05-22 12:48:06 +1000

    Make it so users can save custom short_url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,11 @@
 class Link < ApplicationRecord
-  before_save { self.short_url = SecureRandom.urlsafe_base64(6) }
+  before_validation { check_short_url }
 
   belongs_to :user
+
+  def check_short_url
+    unless self.short_url
+      self.short_url = SecureRandom.urlsafe_base64(6)
+    end
+  end
 end
